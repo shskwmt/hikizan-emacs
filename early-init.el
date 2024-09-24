0a30a3047d215550c447c78760176be2ff9d4f20
shskwmt <shskwmt@outlook.jp>
2024-09-24 13:41:26 +0900

    feat: add emacsenv

--- early-init.el
+++ early-init.el
@@ -1,4 +1,9 @@
 ;;; early-init.el --- -*- lexical-binding: t; -*-
 
+;; set environment variables
+(defvar env-file-path "~/.emacsenv")
+(if (file-readable-p env-file-path)
+  (load-file env-file-path))
+
 ;; disable package.el
 (setq package-enable-at-startup nil)
