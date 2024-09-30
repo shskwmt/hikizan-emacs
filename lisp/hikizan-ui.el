b460aec2d007e9993565fccafb99ef80a5d21e39
shskwmt <shskwmt@outlook.jp>
2024-09-30 21:54:37 +0900

    feat(ui): change color theme

--- lisp/hikizan-ui.el
+++ lisp/hikizan-ui.el
@@ -1,15 +1,12 @@
 ;;; hikizan-ui.el --- ui  -*- lexical-binding: t; -*-
 
-(load-theme 'whiteboard)
+(load-theme 'modus-vivendi)
 
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 
 (which-function-mode t)
-
-(global-hl-line-mode t)
-(set-face-background 'hl-line "alice blue")
 
 (setq confirm-kill-emacs 'y-or-n-p)
 (setq ring-bell-function #'ignore)
