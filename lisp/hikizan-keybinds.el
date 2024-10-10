31cd120910ead2b47a750286e06ebe211303619c
shskwmt <shskwmt@outlook.jp>
2024-10-11 08:53:22 +0900

    feat(keybinds): add key bindings to adjust text scale

--- lisp/hikizan-keybinds.el
+++ lisp/hikizan-keybinds.el
@@ -210,6 +210,8 @@
 (global-set-key (kbd "C-x b") 'consult-buffer)
 (global-set-key (kbd "M-y") 'consult-yank-from-kill-ring)
 (global-set-key (kbd "C-h C-i") 'consult-info)
+(global-set-key (kbd "C->") 'text-scale-increase)
+(global-set-key (kbd "C-<") 'text-scale-decrease)
 
 ;;; mode key bindings
 
