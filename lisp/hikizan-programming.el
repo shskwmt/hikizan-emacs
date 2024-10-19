dceaf467c6fd39a8bc4e108ced945de4eba9d63d
shskwmt <shskwmt@outlook.jp>
2024-10-19 23:24:15 +0900

    feat(programming): add lsp-mode

--- lisp/hikizan-programming.el
+++ lisp/hikizan-programming.el
@@ -11,6 +11,21 @@
 (use-package terraform-mode
   :ensure t)
 
+;;; lsp
+(use-package lsp-mode
+  :ensure t
+  :init
+  (setq lsp-keymap-prefix "C-c l")
+  :hook
+  (ruby-mode . lsp)
+  (go-mode . lsp)
+  (lsp-mode . lsp-enable-which-key-integration)
+  :commands (lsp))
+
+(use-package lsp-ui
+  :ensure t
+  :commands (lsp-ui-mode))
+
 ;;; ein https://github.com/millejoh/emacs-ipython-notebook
 (use-package ein
   :ensure t)
