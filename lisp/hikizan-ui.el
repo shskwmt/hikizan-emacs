df26d06dbc71cd048a6916138ee94350a0ac7ac4
shskwmt <shskwmt@outlook.jp>
2024-10-19 23:24:37 +0900

    feat(ui): set fonts

--- lisp/hikizan-ui.el
+++ lisp/hikizan-ui.el
@@ -1,5 +1,26 @@
 ;;; hikizan-ui.el --- ui  -*- lexical-binding: t; -*-
 
+(add-to-list 'default-frame-alist '(font . "NotoMono NF-12.0"))
+(setq use-default-font-for-symbols nil)
+
+(use-package nerd-icons
+  :ensure t)
+
+(use-package nerd-icons-dired
+  :ensure t
+  :hook
+  (dired-mode . nerd-icons-dired-mode))
+
+(use-package nerd-icon-corfu
+  :ensure t)
+
+(use-package nerd-icons-completion
+  :ensure t
+  :after marginalia
+  :config
+  (nerd-icons-completion-mode)
+  (add-hook 'marginalia-mode-hook #'nerd-icons-completion-marginalia-setup))
+
 (use-package minimal-theme
   :ensure t
   :config
