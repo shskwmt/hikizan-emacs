4afb3c9e3c976d79a493fbef47a6e0134908ac0f
shskwmt <shskwmt@outlook.jp>
2025-02-21 19:58:39 +0900

    feat: Remove magit

--- lisp/hikizan-project.el
+++ lisp/hikizan-project.el
@@ -1,15 +1,5 @@
 ;;; hikizan-project.el --- project  -*- lexical-binding: t; -*-
 
-;; https://github.com/magit/transient
-(use-package transient
-  :ensure t)
-
-;; https://magit.vc/
-(use-package magit
-  :ensure t
-  :bind
-  (("C-x g" . magit-status)))
-
 ;; https://github.com/emacsorphanage/git-gutter
 (use-package git-gutter
   :ensure t
