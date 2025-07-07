5a4cab1f95060df16a9c689bc12a9f23a26526db
shskwmt <shskwmt@outlook.jp>
2025-07-07 22:56:46 +0900

    refactor(keybinds): Remove Evil mode configuration
    This commit removes the `evil` and `evil-collection` configurations from `lisp/hikizan-keybinds.el`. This change indicates a shift away from using Evil mode for keybindings in this Emacs configuration.

--- lisp/hikizan-keybinds.el
+++ lisp/hikizan-keybinds.el
@@ -57,33 +57,6 @@
   (which-key-setup-side-window-bottom)
   (which-key-mode))
 
-;;; evil Configuration
-(use-package evil
-  :ensure t
-  :init
-  (setq evil-want-integration t)
-  (setq evil-want-keybinding nil)
-  :config
-  ;; change mode-line color by evil state
-  (let ((default-color (cons (face-background 'mode-line)
-                             (face-foreground 'mode-line))))
-    (add-hook 'post-command-hook
-	      (lambda ()
-		(let ((color (cond ((minibufferp) default-color)
-				   ((evil-insert-state-p) '("#e80000" . "#ffffff"))
-				   ((evil-emacs-state-p)  '("#444488" . "#ffffff"))
-				   ((buffer-modified-p)   '("#006fa0" . "#ffffff"))
-				   (t default-color))))
-		  (set-face-background 'mode-line (car color))
-		  (set-face-foreground 'mode-line (cdr color))))))
-  (evil-mode 1))
-
-(use-package evil-collection
-  :after evil
-  :ensure t
-  :config
-  (evil-collection-init))
-
 (provide 'hikizan-keybinds)
 
 ;;; hikizan-keybinds.el ends here
