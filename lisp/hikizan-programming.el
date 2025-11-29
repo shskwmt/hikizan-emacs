9c072106c59b636d888961e218daf960af5fca00
shskwmt <shskwmt@outlook.jp>
2025-11-30 00:29:11 +0900

    refactor(ruby): Replace ruby-ts-mode with ruby-mode
    Removes the treesit configuration for Ruby files and reverts to the
    standard `ruby-mode`.
    
    This simplifies the setup by removing the dependency on the Ruby
    treesit grammar and associated configurations. The mode hook is
    updated accordingly to ensure `.rb` files now open with `ruby-mode`.

--- lisp/hikizan-programming.el
+++ lisp/hikizan-programming.el
@@ -6,37 +6,9 @@
   :config
   (add-hook 'after-init-hook #'global-flycheck-mode))
 
-;; treesit
-(setq treesit-language-source-alist
-   '((cmake "https://github.com/uyha/tree-sitter-cmake")
-     (css "https://github.com/tree-sitter/tree-sitter-css")
-     (ruby "https://github.com/tree-sitter/tree-sitter-ruby")
-     (elisp "https://github.com/Wilfred/tree-sitter-elisp")
-     ;;(go "https://github.com/tree-sitter/tree-sitter-go")
-     (gomod "https://github.com/camdencheek/tree-sitter-go-mod")
-     (html "https://github.com/tree-sitter/tree-sitter-html")
-     ;;(javascript "https://github.com/tree-sitter/tree-sitter-javascript" "master" "src")
-     (json "https://github.com/tree-sitter/tree-sitter-json")
-     (make "https://github.com/alemuller/tree-sitter-make")
-     (markdown "https://github.com/ikatyang/tree-sitter-markdown")
-     ;;(python "https://github.com/tree-sitter/tree-sitter-python")
-     (toml "https://github.com/tree-sitter/tree-sitter-toml")
-     (tsx "https://github.com/tree-sitter/tree-sitter-typescript" "master" "tsx/src")
-     (typescript "https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src")
-     (yaml "https://github.com/ikatyang/tree-sitter-yaml")))
-
-(dolist (element treesit-language-source-alist)
-  (let* ((lang (car element)))
-    (if (treesit-language-available-p lang)
-        (message "treesit: %s is already installed" lang)
-      (message "treesit: %s is not installed" lang)
-      (treesit-install-language-grammar lang))))
-
 ;; ruby
-(use-package ruby-ts-mode
-  :mode
-  (("\\.rb$" . ruby-ts-mode)
-   ("\\.rbs$" . ruby-ts-mode)))
+(use-package ruby-mode
+  :ensure t)
 
 ;; go
 (use-package go-mode
